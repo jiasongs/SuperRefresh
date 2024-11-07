a53bebd108d672eb09fcec7a8cdce426bf6b4a68
jiasong <593908937@qq.com>
2024-11-07 13:38:27 +0800

    no message

--- Example/SuperRefreshExample/Sources/ViewController.swift
+++ Example/SuperRefreshExample/Sources/ViewController.swift
@@ -8,10 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private lazy var tableView: UITableView = {
+        let view = UITableView()
+        view.contentInsetAdjustmentBehavior = .never
+        view.delegate = self
+        view.dataSource = self
+        view.register(UITableViewCell.self, forCellReuseIdentifier: "UITableViewCell")
+        return view
+    }()
+    
+    private lazy var refreshControl: UIRefreshControl = {
+        let view = UIRefreshControl()
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .black
+        self.view.addSubview(self.tableView)
+        
+        self.tableView.refreshControl = self.refreshControl
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        self.tableView.frame = self.view.bounds
+        self.tableView.contentInset = UIEdgeInsets(top: self.view.safeAreaInsets.top, left: 0, bottom: self.view.safeAreaInsets.bottom, right: 0)
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.item)"
+        return cell
+    }
+    
 }
